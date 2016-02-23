1508824e830c059249409e561effd4d73ff4604d
MaramZine <maram_zine@hotmail.com>
2016-02-23 13:54:32 +0200

    aaaa

---
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/MaramActivity.java
@@ -0,0 +1,16 @@
+package com.alsalam.sclzroot.Activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.sclzservice.R;
+
+public class MaramActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maram);
+    }
+
+}
