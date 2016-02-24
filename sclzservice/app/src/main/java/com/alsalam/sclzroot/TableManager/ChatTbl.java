020f4ca72ebaaacbe6ff77bee4cae42cc9708f2e
Wael Abed Elal <worldhack1@hotmail.com>
2016-02-24 08:49:20 +0200

    Merged files

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/TableManager/ChatTbl.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/TableManager/ChatTbl.java
@@ -6,14 +6,17 @@ import java.sql.Date;
  * Created by משתמש on 2/23/2016.
  */
 public class ChatTbl {
+    @com.google.gson.annotations.SerializedName("id")
     private String id;
+    @com.google.gson.annotations.SerializedName("chatMsg")
     private String chatMsg;
+    @com.google.gson.annotations.SerializedName("chatMsgDate")
     private Date   chatMsgDate;
 
     public ChatTbl(String id, String chatMsg, Date chatMsgDate) {
-        this.id = id;
-        this.chatMsg = chatMsg;
-        this.chatMsgDate = chatMsgDate;
+        setId(id);
+        setChatMsg(chatMsg);
+        setChatMsgDate(chatMsgDate);
     }
 
     public String getId() {
