83f597d17480ea66180ee7d3ba863b6238109cb7
yara1997 <yarabadran@gmail.com>
2016-02-24 09:40:53 +0200

    yyyy

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/AddTaskActivity.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/AddTaskActivity.java
@@ -11,7 +11,7 @@ public class AddTaskActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
-
+        //kjljklkljj
     }
 
 }
