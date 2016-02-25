7bd9f5e3252c753f6010b18184af895e4f729d5a
bahaamarshud <bmarshoud74@gmail.com>
2016-02-25 11:37:55 +0200

    done

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/RegisterActivity.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.alsalam.sclzroot.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -39,7 +41,22 @@ public class RegisterActivity extends Activity {
         rdbM=(RadioButton)findViewById(R.id.rdbM);// choosig female or male
 
         btnSubmit=(Button)findViewById(R.id.btnSubmit);
-     }
+
+
+         btnSubmit.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                  switch (v.getId()){
+                      case R.id.btnSubmit:
+                          Intent intent =new Intent(RegisterActivity.this,HomeActivity.class);
+                          startActivity(intent);
+                          break;
+
+                  }
+             }
+         });
+
+         }
 
 
 
