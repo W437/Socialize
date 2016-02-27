daa6c09fbf7b441428f06390de92b3f75800ff8a
samih1079 <samih1079@gmail.com>
2016-02-27 13:46:59 +0200

    new hompage with pgaer and tabs new theme fragments

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/HomeMapActivity.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/HomeMapActivity.java
@@ -81,8 +81,10 @@ public class HomeMapActivity extends FragmentActivity implements OnMapReadyCallb
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng sydney = new LatLng(32.9943511, 35.1472984);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("מקיף השלום"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15));
     }
 }
---
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/MainHomeActivity.java
@@ -0,0 +1,86 @@
+package com.alsalam.sclzroot.Activities;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.alsalam.sclzroot.MyFragments.MapListFragment;
+import com.alsalam.sclzroot.MyFragments.Profile2Fragment;
+import com.example.sclzservice.R;
+
+public class MainHomeActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    Fragment[] fragments;
+    MyPagerAdatpter myPagerAdatpter;
+    TabLayout tabLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home);
+        viewPager= (ViewPager) findViewById(R.id.homepager);
+        tabLayout= (TabLayout) findViewById(R.id.tabs);
+
+
+
+
+        fragments=new Fragment[2];
+        fragments[0]=new MapListFragment();
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_action_map));
+        fragments[1]=new Profile2Fragment();
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_action_profile));
+
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+
+
+        myPagerAdatpter=new MyPagerAdatpter(getSupportFragmentManager(),fragments);
+        viewPager.setAdapter(myPagerAdatpter);
+
+
+
+    }
+
+    private  static class MyPagerAdatpter extends FragmentPagerAdapter
+    {
+        Fragment[] fragments;
+        public MyPagerAdatpter(FragmentManager fm,Fragment[] fragments) {
+            super(fm);
+            this.fragments=fragments;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            return fragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return null==fragments ? 0:fragments.length;
+        }
+    }
+}
