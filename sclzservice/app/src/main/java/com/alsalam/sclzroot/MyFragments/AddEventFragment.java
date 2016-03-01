6b61ecccb070858bf6812d49b6be71889050a26e
samih1079 <samih1079@gmail.com>
2016-03-01 21:33:45 +0200

    aaaa

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/MyFragments/AddEventFragment.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/MyFragments/AddEventFragment.java
@@ -21,7 +21,6 @@ public class AddEventFragment extends Fragment {
      private  Button btnDone;
      private RadioButton rdb_in,rdb_out,rdb_male,rdb_female;
      private Spinner spnType;
-//lklk;kjjjllll
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
