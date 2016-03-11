704f87f506ff5c7c0879c2b9607876f06fb0d070
Wael <waelisnothing@gmail.com>
2016-03-11 16:24:49 +0200

    Small tweaks

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/MainHomeActivity.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/MainHomeActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
