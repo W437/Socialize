48f72258651f91c4ccb0a3cbf018ce7fe4a49c78
Wael <waelisnothing@gmail.com>
2016-03-15 13:11:30 +0200

    ...

---
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/KKK.java
@@ -0,0 +1,7 @@
+package com.alsalam.sclzroot.Activities;
+
+/**
+ * Created by Wael on 15/03/16.
+ */
+public class KKK {
+}
--- sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/LLL.java
+++
@@ -1,7 +0,0 @@
-package com.alsalam.sclzroot.Activities;
-
-/**
- * Created by samih on 10/03/2016.
- */
-public class LLL {
-}
