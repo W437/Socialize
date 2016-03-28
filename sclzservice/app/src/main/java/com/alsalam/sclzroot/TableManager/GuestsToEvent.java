9a3ee017e546cec85982c021d2e58e0f6faacbed
MaramZine <maram_zine@hotmail.com>
2016-03-28 13:59:37 +0300

    hebrew stringse

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/TableManager/GuestsToEvent.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/TableManager/GuestsToEvent.java
@@ -7,25 +7,26 @@ public class GuestsToEvent {
 
     @com.google.gson.annotations.SerializedName("id")
     private String id;
+    @com.google.gson.annotations.SerializedName("eventid")
+
     private String eventid;
+    @com.google.gson.annotations.SerializedName("userId")
+
     private String userId;//Guest
+    @com.google.gson.annotations.SerializedName("Status")
+
     private String Status= EventTbl.WAITING;
+    @com.google.gson.annotations.SerializedName("iNeed")
 
-    public GuestsToEvent(String id, String eventid, String userId, String status) {
-        this.id = id;
-        this.eventid = eventid;
-        this.userId = userId;
-        Status = status;
-    }
+    private String iNeed;
+    @com.google.gson.annotations.SerializedName("iBring")
 
-    public void setStatus(String status) {
-        Status = status;
-    }
+    private  String iBring;
+    @com.google.gson.annotations.SerializedName("countP")
 
-    public String getStatus() {
+    private String countP;
 
-        return Status;
-    }
+
 
     public GuestsToEvent() {
     }
@@ -52,6 +53,45 @@ public class GuestsToEvent {
 
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+    public String getiNeed() {
+        return iNeed;
+    }
+
+    public void setiNeed(String iNeed) {
+        this.iNeed = iNeed;
+    }
+
+    public String getiBring() {
+        return iBring;
+    }
+
+    public void setiBring(String iBring) {
+        this.iBring = iBring;
+    }
+
+    public String getCountP() {
+        return countP;
+    }
+
+    public void setCountP(String countP) {
+        this.countP = countP;
+    }
+
+    public GuestsToEvent(String id, String eventid, String userId, String status) {
+        this.id = id;
+        this.eventid = eventid;
+        this.userId = userId;
+        Status = status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getStatus() {
+
+        return Status;
     }
 
     @Override
