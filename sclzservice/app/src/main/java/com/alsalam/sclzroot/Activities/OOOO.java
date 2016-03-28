ea6ca0d6c409a71b504df20303d778e38176da02
samih1079 <samih1079@gmail.com>
2016-03-29 00:56:53 +0300

    Package name changed New api key according the new pacage name push notification working to all

---
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/OOOO.java
@@ -0,0 +1,7 @@
+package com.alsalam.sclzroot.Activities;
+
+/**
+ * Created by samih on 29/03/2016.
+ */
+public class OOOO {
+}
--- sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/kkkkk.java
+++
@@ -1,7 +0,0 @@
-package com.alsalam.sclzroot.Activities;
-
-/**
- * Created by hp1 on 28/03/2016.
- */
-public class kkkkk {
-}
