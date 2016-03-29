81efd52ea91dcef8eba11a3c32bf46ade9b70f82
yara1997 <yarabadran@gmail.com>
2016-03-29 21:51:28 +0300

    spinner 12

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/UUU.java
+++
@@ -1,7 +0,0 @@
-package com.alsalam.sclzroot.Activities;
-
-/**
- * Created by samih on 29/03/2016.
- */
-public class UUU {
-}
--- sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/hhhh.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/hhhh.java
@@ -3,5 +3,5 @@ package com.alsalam.sclzroot.Activities;
 /**
  * Created by bana on 29/03/2016.
  */
-public class saa {
+public class hhhh {
 }
