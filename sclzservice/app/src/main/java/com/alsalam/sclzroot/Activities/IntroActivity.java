9d80727f44f701c0bfe70436794a24b520633d94
Wael <waelisnothing@gmail.com>
2016-03-31 15:19:32 +0300

    ..

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/IntroActivity.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/IntroActivity.java
@@ -35,7 +35,7 @@ public class IntroActivity extends AppIntro {
         // Just set a title, description, background and image. AppIntro will do the rest.
         addSlide(AppIntroFragment.newInstance("Welcome to Socialize.", "Meeting people and organizing events made fun.", R.drawable.sclzlogo, Color.parseColor("#F44336")));
         addSlide(AppIntroFragment.newInstance("Your Events and Others', all organised!", "Meet people around you and have fun together.", R.drawable.organised, Color.parseColor("#F44336")));
-        addSlide(AppIntroFragment.newInstance("Endless Fun", "With the variety of events to choose from, you'll always be entertained!", R.drawable.happy, Color.parseColor("#F44336")));
+        addSlide(AppIntroFragment.newInstance("Endless Fun.", "With the variety of events to choose from, you'll always be entertained!", R.drawable.happy, Color.parseColor("#F44336")));
         addSlide(AppIntroFragment.newInstance("Socialize Yourself in One Click.", "With just a few clicks, you can find a suitable event to participate in, near you.", R.drawable.connected, Color.parseColor("#F44336")));
         addSlide(AppIntroFragment.newInstance("Everything and Anything.", "Whether it's a football game, a good-deeds-day, a meeting session to discuss relevant daily stuff with people from different cultures, you can find anything!", R.drawable.everything, Color.parseColor("#F44336")));
         addSlide(AppIntroFragment.newInstance("You are all set. Enjoy Socialize.", "Take a minute to register and get started.", R.drawable.ready, Color.parseColor("#F44336")));
