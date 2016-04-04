d2aea2612e408be901f6e1da6b7b080873f9c49d
Wael <waelisnothing@gmail.com>
2016-04-04 14:29:56 +0300

    Fixes

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/MyFragments/MapListFragment.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/MyFragments/MapListFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
