1865a72683ab2ab68c96006f61104498cc89dd9f
Wael <waelisnothing@gmail.com>
2016-04-04 14:29:28 +0300

    Fixes

--- sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/MainpageActivity.java
+++ sclzservice/app/src/main/java/com/alsalam/sclzroot/Activities/MainpageActivity.java
@@ -1,5 +1,6 @@
 package com.alsalam.sclzroot.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -278,7 +279,8 @@ public class MainpageActivity extends AppCompatActivity
         {
 
             DataBaseMngr.logOut(this);
-            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+            //finish();
 
 
         }
